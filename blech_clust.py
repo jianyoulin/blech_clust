56df981318eef937704813038d36dff52e14f31d
Jian-You Lin <jianyoulin@gmail.com>
2024-05-28 11:30:38 -0400

    adding parallel code to run blech_process parallel in Widnows machine

--- blech_clust.py
+++ blech_clust.py
@@ -10,6 +10,7 @@ from pathlib import Path
 
 # Necessary blech_clust modules
 import read_file
+from write_file import make_powershell_parallel_script
 
 # get path for blech_clust folder
 blech_clust_dir = os.getcwd()
@@ -17,6 +18,8 @@ blech_clust_dir = os.getcwd()
 # Get name of directory with the data files
 try:
     dir_name = sys.argv[1]
+    if dir_name == '-f':
+        dir_name = easygui.diropenbox('Select the dir path where data are saved')
 except:
     dir_name = easygui.diropenbox('Select the dir path where data are saved')
 
@@ -200,24 +203,17 @@ channel_map_df.to_csv(os.path.join(dir_name, 'channel_map.csv'))
 # Make a directory for dumping files talking about memory usage in blech_process.py
 os.mkdir('memory_monitor_clustering')
 
-# Ask for the HPC queue to use - was in previous version, now just use all.q
-clustering = easygui.multchoicebox(msg = 'Which method do you want to use for clustering waveforms?', choices = ('PCA', 'UMAP'))
+# # Ask for the HPC queue to use - was in previous version, now just use all.q
+# clustering = easygui.multchoicebox(msg = 'Which method do you want to use for clustering waveforms?', choices = ('PCA', 'UMAP'))
 
-# Grab Brandeis unet username
-username = easygui.multenterbox(msg = 'Enter your Brandeis/Jetstream/personal computer id', fields = ['unet username'])
+# # Grab Brandeis unet username
+# username = easygui.multenterbox(msg = 'Enter your Brandeis/Jetstream/personal computer id', fields = ['unet username'])
 
 # Dump shell file for running array job on the user's blech_clust folder on the desktop
 try:
     os.chdir(blech_clust_dir) #'/home/%s/Desktop/blech_clust' % username[0])
 except:
     os.chdir('/mnt/c/Users/jiany/Desktop/blech_clust')
-
-f = open('blech_clust.sh', 'w')
-print("export OMP_NUM_THREADS=1", file = f)
-print("cd {}".format(os.getcwd()), file=f)
-#print("cd /home/%s/Desktop/blech_clust" % username[0], file=f)
-print("python blech_process.py", file=f)
-f.close()
 
 # Dump shell file(s) for running GNU parallel job on the user's blech_clust folder on the desktop
 # First get number of CPUs - parallel be asked to run num_cpu-1 threads in parallel
@@ -239,26 +235,46 @@ num_cpu = multiprocessing.cpu_count()//2 # number of cpu cores to be used in clu
 # f.close()
 all_electrodes = np.concatenate(tuple(e_channels[k] for k in e_channels.keys()))
 
-f = open('blech_clust_jetstream_parallel.sh', 'w')
-print("parallel -k -j {:d} --noswap --load 100% --progress --memfree 4G --retry-failed "\
-        "--joblog {:s}{}results.log bash blech_clust_jetstream_parallel1.sh ::: {{{}}}"\
-        .format(num_cpu, dir_name, os.path.sep, ",".join([str(x) for x in all_electrodes]))
-        , file = f)
-f.close()
+if 'win' in sys.platform:
+    # make parallel script to clustering waveforms with multiple cores
+    make_powershell_parallel_script(electrodes = all_electrodes, 
+                                    num_cpu = num_cpu, 
+                                    process_path = blech_clust_dir, 
+                                    process_code = 'blech_process.py')
+    # make parallel script to plot spike scatters using umap with multiple cores
+    make_powershell_parallel_script(electrodes = all_electrodes, 
+                                    num_cpu = num_cpu, 
+                                    process_path = blech_clust_dir, 
+                                    process_code = 'umap_spike_scatter_parallel.py')
+    
+else:
+    f = open('blech_clust.sh', 'w')
+    print("export OMP_NUM_THREADS=1", file = f)
+    print("cd {}".format(os.getcwd()), file=f)
+    #print("cd /home/%s/Desktop/blech_clust" % username[0], file=f)
+    print("python blech_process.py", file=f)
+    f.close()
 
-# Then produce the file that runs blech_process.py
-f = open('blech_clust_jetstream_parallel1.sh', 'w')
-print("export OMP_NUM_THREADS=1", file = f)
-print("python blech_process.py $1 {}".format(clustering[0]), file = f)
-f.close()
+    f = open('blech_clust_jetstream_parallel.sh', 'w')
+    print("parallel -k -j {:d} --noswap --load 100% --progress --memfree 4G --retry-failed "\
+            "--joblog {:s}{}results.log bash blech_clust_jetstream_parallel1.sh ::: {{{}}}"\
+            .format(num_cpu, dir_name, os.path.sep, ",".join([str(x) for x in all_electrodes]))
+            , file = f)
+    f.close()
 
-# produce bash file to run umap_spike_scatter and parallelize them
-f = open('bash_umap_spike_scatter.sh', 'w')
-print("parallel -k -j {:d} --load 100% --progress --memfree 4G --retry-failed "\
-"python umap_spike_scatter_parallel.py ::: {{{}}}"\
-    .format(num_cpu, ",".join([str(x) for x in all_electrodes])),
- file = f)
-f.close()
+    # Then produce the file that runs blech_process.py
+    f = open('blech_clust_jetstream_parallel1.sh', 'w')
+    print("export OMP_NUM_THREADS=1", file = f)
+    print("python blech_process.py $1 {}".format(clustering[0]), file = f)
+    f.close()
+
+    # produce bash file to run umap_spike_scatter and parallelize them
+    f = open('bash_umap_spike_scatter.sh', 'w')
+    print("parallel -k -j {:d} --load 100% --progress --memfree 4G --retry-failed "\
+    "python umap_spike_scatter_parallel.py ::: {{{}}}"\
+        .format(num_cpu, ",".join([str(x) for x in all_electrodes])),
+     file = f)
+    f.close()
 
 # Dump the directory name where blech_process has to cd
 f = open('blech.dir', 'w')
@@ -270,12 +286,58 @@ f = open('umap_dir.txt', 'w')
 print(dir_name, file=f)
 f.close()
 
-print("Now logout of the compute node and go back to the login node. Then go to the bkech_clust folder on your desktop and say: qsub -t 1-"+str(len(ports)*32-len(emg_channels))+" -q all.q -ckpt reloc -l mem_free=4G -l mem_token=4G blech_clust.sh")
+if 'win' in sys.platform:
+    print('Since you are using Windows OS, please do the following for sorting processing')
+    print('Next: run .\blech_clust_PS_parallel.ps1 in Powershell 7.X terminal')
+    print('Then: run .\blech_clust_spike_scatter_PS_parallel.ps1 in Powershell 7.X terminal')
+
+else:
+    print('cd to blech_clust directory')
+    print('Next: bash blech_clust_jetstream_parallel.sh')
+    print('Then: bash bash_umap_spike_scatter.sh')
+    
+# print("Now logout of the compute node and go back to the login node. Then go to the bkech_clust folder on your desktop and say: qsub -t 1-"+str(len(ports)*32-len(emg_channels))+" -q all.q -ckpt reloc -l mem_free=4G -l mem_token=4G blech_clust.sh")
 
 
 
 
-
+def make_powershell_parallel_script(electrodes = None, num_cpu = None, process_path = None, process_code = None):
+    """
+    electrodes:: a list of electrode numbers (int)
+    """
+    n_cores_to_be_used = num_cpu
+    path_ = os.path.join(process_path, process_code)
+    if 'umap' in process_code:
+        f = open('blech_clust_spike_scatter_PS_parallel.ps1', 'w')
+    else:
+        f = open('blech_clust_PS_parallel.ps1', 'w')
+    print("# Define the scripts and their arguments",  file = f)
+    print("$scripts = @(", file = f)
+    for i in electrodes:
+        print('\t@{Path = "%s"; Args = @("%i")}' % (path_, i), file=f)
+    print(")", file = f)
+    print('\n', file = f)
+    print("# Define the throttle limit",  file = f)
+    print('$throttleLimit = %i' % n_cores_to_be_used, file = f)
+    print('\n', file = f)
+    print('# Start the jobs with throttle limit', file = f)
+    print('$jobs = foreach ($script in $scripts) {', file=f)
+    print('\tStart-ThreadJob -ScriptBlock {', file=f)
+    print('\t\tparam ($scriptPath, $scriptArgs)', file=f)
+    print('\t\tpython $scriptPath $scriptArgs', file=f)
+    print('\t} -ArgumentList $script.Path, ($script.Args -join " ") -ThrottleLimit $throttleLimit', file=f)
+    print('}', file=f)
+    print('\n', file = f)
+    print('$jobs | ForEach-Object { $_ | Wait-Job }', file=f)
+    print('$jobs | ForEach-Object {', file=f)
+    print('\t$output = $_ | Receive-Job', file=f)
+    print('\tWrite-Output "Output from $($_.Name):"', file=f)
+    print('\tWrite-Output $output', file=f)
+    print('}', file=f)
+    print('\n', file = f)
+    print('# Clean up the jobs', file=f)
+    print('$jobs | ForEach-Object { $_ | Remove-Job }', file=f)
+    f.close()
 
 
 
