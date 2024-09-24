ba678e894ea67c499667be7c365d56bb80f50f46
Jian-You Lin <jianyoulin@gmail.com>
2024-09-24 10:20:10 -0400

    regular updates

--- blech_clust.py
+++ blech_clust.py
@@ -245,7 +245,7 @@ if 'win' in sys.platform:
     make_powershell_parallel_script(electrodes = all_electrodes, 
                                     num_cpu = num_cpu, 
                                     process_path = blech_clust_dir, 
-                                    process_code = 'umap_spike_scatter_parallel.py')
+                                    process_code = 'umap_spike_scatter.py')
     
 else:
     f = open('blech_clust.sh', 'w')
@@ -288,8 +288,9 @@ f.close()
 
 if 'win' in sys.platform:
     print('Since you are using Windows OS, please do the following for sorting processing')
-    print('Next: run .\blech_clust_PS_parallel.ps1 in Powershell 7.X terminal')
-    print('Then: run .\blech_clust_spike_scatter_PS_parallel.ps1 in Powershell 7.X terminal')
+    print('Next: run blech_common_avg_reference.py in Powershell 7.X terminal')
+    print('Next: run blech_clust_PS_parallel.ps1 in Powershell 7.X terminal')
+    print('Then: run blech_clust_spike_scatter_PS_parallel.ps1 in Powershell 7.X terminal')
 
 else:
     print('cd to blech_clust directory')
@@ -301,43 +302,43 @@ else:
 
 
 
-def make_powershell_parallel_script(electrodes = None, num_cpu = None, process_path = None, process_code = None):
-    """
-    electrodes:: a list of electrode numbers (int)
-    """
-    n_cores_to_be_used = num_cpu
-    path_ = os.path.join(process_path, process_code)
-    if 'umap' in process_code:
-        f = open('blech_clust_spike_scatter_PS_parallel.ps1', 'w')
-    else:
-        f = open('blech_clust_PS_parallel.ps1', 'w')
-    print("# Define the scripts and their arguments",  file = f)
-    print("$scripts = @(", file = f)
-    for i in electrodes:
-        print('\t@{Path = "%s"; Args = @("%i")}' % (path_, i), file=f)
-    print(")", file = f)
-    print('\n', file = f)
-    print("# Define the throttle limit",  file = f)
-    print('$throttleLimit = %i' % n_cores_to_be_used, file = f)
-    print('\n', file = f)
-    print('# Start the jobs with throttle limit', file = f)
-    print('$jobs = foreach ($script in $scripts) {', file=f)
-    print('\tStart-ThreadJob -ScriptBlock {', file=f)
-    print('\t\tparam ($scriptPath, $scriptArgs)', file=f)
-    print('\t\tpython $scriptPath $scriptArgs', file=f)
-    print('\t} -ArgumentList $script.Path, ($script.Args -join " ") -ThrottleLimit $throttleLimit', file=f)
-    print('}', file=f)
-    print('\n', file = f)
-    print('$jobs | ForEach-Object { $_ | Wait-Job }', file=f)
-    print('$jobs | ForEach-Object {', file=f)
-    print('\t$output = $_ | Receive-Job', file=f)
-    print('\tWrite-Output "Output from $($_.Name):"', file=f)
-    print('\tWrite-Output $output', file=f)
-    print('}', file=f)
-    print('\n', file = f)
-    print('# Clean up the jobs', file=f)
-    print('$jobs | ForEach-Object { $_ | Remove-Job }', file=f)
-    f.close()
+# def make_powershell_parallel_script(electrodes = None, num_cpu = None, process_path = None, process_code = None):
+#     """
+#     electrodes:: a list of electrode numbers (int)
+#     """
+#     n_cores_to_be_used = num_cpu
+#     path_ = os.path.join(process_path, process_code)
+#     if 'umap' in process_code:
+#         f = open('blech_clust_spike_scatter_PS_parallel.ps1', 'w')
+#     else:
+#         f = open('blech_clust_PS_parallel.ps1', 'w')
+#     print("# Define the scripts and their arguments",  file = f)
+#     print("$scripts = @(", file = f)
+#     for i in electrodes:
+#         print('\t@{Path = "%s"; Args = @("%i")}' % (path_, i), file=f)
+#     print(")", file = f)
+#     print('\n', file = f)
+#     print("# Define the throttle limit",  file = f)
+#     print('$throttleLimit = %i' % n_cores_to_be_used, file = f)
+#     print('\n', file = f)
+#     print('# Start the jobs with throttle limit', file = f)
+#     print('$jobs = foreach ($script in $scripts) {', file=f)
+#     print('\tStart-ThreadJob -ScriptBlock {', file=f)
+#     print('\t\tparam ($scriptPath, $scriptArgs)', file=f)
+#     print('\t\tpython $scriptPath $scriptArgs', file=f)
+#     print('\t} -ArgumentList $script.Path, ($script.Args -join " ") -ThrottleLimit $throttleLimit', file=f)
+#     print('}', file=f)
+#     print('\n', file = f)
+#     print('$jobs | ForEach-Object { $_ | Wait-Job }', file=f)
+#     print('$jobs | ForEach-Object {', file=f)
+#     print('\t$output = $_ | Receive-Job', file=f)
+#     print('\tWrite-Output "Output from $($_.Name):"', file=f)
+#     print('\tWrite-Output $output', file=f)
+#     print('}', file=f)
+#     print('\n', file = f)
+#     print('# Clean up the jobs', file=f)
+#     print('$jobs | ForEach-Object { $_ | Remove-Job }', file=f)
+#     f.close()
 
 
 
