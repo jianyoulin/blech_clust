98adca16b0b6101d21b061705039bbbad922154e
jianyoulin <jianyoulin@gmail.com>
2026-07-13 10:26:18 -0400

    Refactor directory selection and laser condition handling in LFP processing scripts

--- LFP_analysis/LFP_Processing_Final.py
+++ LFP_analysis/LFP_Processing_Final.py
@@ -2,18 +2,25 @@
 import numpy as np
 import tables
 import easygui
-import os
+import os, sys
 
-#Get name of directory where the data files and hdf5 file sits, and change to that directory for processing
-dir_name = easygui.diropenbox()
-os.chdir(dir_name)
+# Get name of directory where the data files and hdf5 file sits, 
+# and change to that directory for processing
+
+try:
+    dir_name = sys.argv[1]
+    if dir_name == '-f':
+        dir_name = easygui.diropenbox('Select the dir path where data are saved')
+except:
+    dir_name = easygui.diropenbox('Select the dir path where data are saved')
+
 
 #Look for the hdf5 file in the directory
 file_list = os.listdir('./')
 hdf5_name = ''
 for files in file_list:
-	if files[-2:] == 'h5':
-		hdf5_name = files
+    if files[-2:] == 'h5':
+        hdf5_name = files
 
 #Open the hdf5 file
 hf5 = tables.open_file(hdf5_name, 'r+')
@@ -28,22 +35,22 @@ electrodegroup = np.unique(electrodegroup)
 noncell_channels = easygui.integerbox(msg='Number of channels', title='Purposes other than cell recording (i.e. EMG)', default='0',lowerbound=0,upperbound=64)
 
 if noncell_channels == 0:		#If all channels are used for cell recording, move on.
-	fieldValues = []
-	
+    fieldValues = []
+    
 else:							#If there are channels used, otherwise: create variable array for channel specification
-	Fields = []
-	for channel in range(noncell_channels):
-		chan_variable = "Channel " + "%01d" % (channel)
-		Fields.append(chan_variable)
+    Fields = []
+    for channel in range(noncell_channels):
+        chan_variable = "Channel " + "%01d" % (channel)
+        Fields.append(chan_variable)
 
-	fieldValues = easygui.multenterbox('Which Channels are these (i.e. EMG)?', 'Used Channels?', Fields) #Specify which channels are used for non-cell recording
-	
-	for i in range(len(Fields)):	#Check to make sure that user input has as many non-cell channel numbers as they indicated were used
-		if fieldValues[i-1].strip() == "":
-			errmsg = "You did not specify as many channels as you indicated you had!"
-			print(errmsg)
-			fieldValues = easygui.multenterbox('Your input did not match number of entries. Pay Attention!', 'Used Channels?', Fields)	#If user messed up, they will be given a chance to correct this
-				
+    fieldValues = easygui.multenterbox('Which Channels are these (i.e. EMG)?', 'Used Channels?', Fields) #Specify which channels are used for non-cell recording
+    
+    for i in range(len(Fields)):	#Check to make sure that user input has as many non-cell channel numbers as they indicated were used
+        if fieldValues[i-1].strip() == "":
+            errmsg = "You did not specify as many channels as you indicated you had!"
+            print(errmsg)
+            fieldValues = easygui.multenterbox('Your input did not match number of entries. Pay Attention!', 'Used Channels?', Fields)	#If user messed up, they will be given a chance to correct this
+                
 EMG_Channels = fieldValues
 
 Raw_Electrodefiles = []			#Create and fill array with appropriate .dat file names according to occupied cell recording channels
@@ -67,21 +74,21 @@ from scipy.signal import butter
 from scipy.signal import filtfilt
 
 #Specify filtering parameters (linear finite impulse response filter) to define filter specificity across electrodes
-Boxes = ['low','high']
-freqparam = easygui.multenterbox('Specify LFP bandpass filtering paramter (assumes 30kHz as sampling rate)', 'Low-Frequency Cut-off (Hz)', Boxes) 
-#freqparam = easygui.integerbox(msg = 'Specify LFP bandpass filtering paramter (assumes 30kHz as sampling rate)', title = 'Low-Frequency Cut-off (Hz)', default='300', lowerbound=0,upperbound=500)
-
+frequency_request = 'Specify LFP bandpass filtering paramter (assumes 30kHz as sampling rate)'
+freqparam = easygui.multenterbox(frequency_request, 
+                                 'Low-Frequency Cut-off (Hz)', 
+                                 ['low','high']) 
 def get_filtered_electrode(data, freq = freqparam, sampling_rate = 30000.0):
-	el = 0.195*(data)
-	m, n = butter(2, [2.0*int(freqparam[0])/sampling_rate, 2.0*int(freqparam[1])/sampling_rate], btype = 'bandpass')
-	filt_el = filtfilt(m, n, el)
-	return filt_el
+    el = 0.195*(data)
+    m, n = butter(2, [2.0*int(freqparam[0])/sampling_rate, 2.0*int(freqparam[1])/sampling_rate], btype = 'bandpass')
+    filt_el = filtfilt(m, n, el)
+    return filt_el
 
 #Check if LFP data is already within file and remove node if so. Create new raw LFP group within H5 file. 
 try:
-	hf5.remove_node('/raw_LFP', recursive = True)
+    hf5.remove_node('/raw_LFP', recursive = True)
 except:
-	pass
+    pass
 hf5.create_group('/', 'raw_LFP')
 
 #Loop through each neuron-recording electrode (from .dat files), filter data, and create array in new LFP node
@@ -99,56 +106,56 @@ dig_in_nodes = hf5.list_nodes('/digital_in')
 dig_in = []
 dig_in_pathname = []
 for node in dig_in_nodes:
-	dig_in_pathname.append(node._v_pathname)
-	exec("dig_in.append(hf5.root.digital_in.%s[:])" % dig_in_pathname[-1].split('/')[-1])
+    dig_in_pathname.append(node._v_pathname)
+    exec("dig_in.append(hf5.root.digital_in.%s[:])" % dig_in_pathname[-1].split('/')[-1])
 dig_in = np.array(dig_in)
 
 # Get the stimulus delivery times - take the end of the stimulus pulse as the time of delivery
 dig_on = []
 for i in range(len(dig_in)):
-	dig_on.append(np.where(dig_in[i,:] == 1)[0])
+    dig_on.append(np.where(dig_in[i,:] == 1)[0])
 change_points = []
 for on_times in dig_on:
-	changes = []
-	for j in range(len(on_times) - 1):
-		if np.abs(on_times[j] - on_times[j+1]) > 30:
-			changes.append(on_times[j])
-	try:
-		changes.append(on_times[-1]) # append the last trial which will be missed by this method
-	except:
-		pass # Continue without appending anything if this port wasn't on at all
-	change_points.append(changes)
+    changes = []
+    for j in range(len(on_times) - 1):
+        if np.abs(on_times[j] - on_times[j+1]) > 30:
+            changes.append(on_times[j])
+    try:
+        changes.append(on_times[-1]) # append the last trial which will be missed by this method
+    except:
+        pass # Continue without appending anything if this port wasn't on at all
+    change_points.append(changes)
 
 # Show the user the number of trials on each digital input channel, and ask them to confirm
 check = easygui.ynbox(msg = 'Digital input channels: ' + str(dig_in_pathname) + '\n' + 'No. of trials: ' + str([len(changes) for changes in change_points]), title = 'Check and confirm the number of trials detected on digital input channels')
 
 # Go ahead only if the user approves by saying yes
 if check:
-	pass
+    pass
 else:
-	print("Well, if you don't agree, blech_clust can't do much!")
-	sys.exit()
+    print("Well, if you don't agree, blech_clust can't do much!")
+    sys.exit()
 
 # Ask the user which digital input channels should be used for slicing out LFP arrays, and convert the channel numbers into integers for pulling stuff out of change_points
 dig_in_channels = easygui.multchoicebox(msg = 'Which digital input channels should be used to slice out LFP data trial-wise?', choices = ([path for path in dig_in_pathname]))
 dig_in_channel_nums = []
 for i in range(len(dig_in_pathname)):
-	if dig_in_pathname[i] in dig_in_channels:
-		dig_in_channel_nums.append(i)
+    if dig_in_pathname[i] in dig_in_channels:
+        dig_in_channel_nums.append(i)
 
 # Ask the user for the pre and post stimulus durations to be pulled out, and convert to integers
 durations = easygui.multenterbox(msg = 'What are the signal durations pre and post stimulus that you want to pull out', fields = ['Pre stimulus (ms)', 'Post stimulus (ms)'])
 for i in range(len(durations)):
-	durations[i] = int(durations[i])
+    durations[i] = int(durations[i])
 
 # Grab the names of the arrays containing LFP recordings
 lfp_nodes = hf5.list_nodes('/raw_LFP')
 
 # Make the Parsed_LFP node in the hdf5 file if it doesn't exist, else move on
 try:
-	hf5.remove_node('/Parsed_LFP', recursive = True)
+    hf5.remove_node('/Parsed_LFP', recursive = True)
 except:
-	pass
+    pass
 hf5.create_group('/', 'Parsed_LFP')
 
 # Ask if this analysis is looking at more than 1 trial and/or taste
@@ -158,18 +165,18 @@ trial_check = easygui.buttonbox(msg,choices = ["Yes","No"])
 # Run through the tastes if user said there are more than 1 trial
 if trial_check == "Yes":
     for i in range(len(dig_in_channels)):
-        	num_electrodes = len(lfp_nodes) 
-        	num_trials = len(change_points[dig_in_channel_nums[i]])
-        	this_taste_LFPs = np.zeros((num_electrodes, num_trials, durations[0] + durations[1]))
-        	for electrode in range(num_electrodes):
-        		for j in range(len(change_points[dig_in_channel_nums[i]])):
-        			this_taste_LFPs[electrode, j, :] = np.mean(lfp_nodes[electrode][change_points[dig_in_channel_nums[i]][j] - durations[0]*30:change_points[dig_in_channel_nums[i]][j] + durations[1]*30].reshape((-1, 30)), axis = 1)
-        	
-        	print (float(i)/len(dig_in_channels)) #Shows progress	
+        num_electrodes = len(lfp_nodes) 
+        num_trials = len(change_points[dig_in_channel_nums[i]])
+        this_taste_LFPs = np.zeros((num_electrodes, num_trials, durations[0] + durations[1]))
+        for electrode in range(num_electrodes):
+            for j in range(len(change_points[dig_in_channel_nums[i]])):
+                this_taste_LFPs[electrode, j, :] = np.mean(lfp_nodes[electrode][change_points[dig_in_channel_nums[i]][j] - durations[0]*30:change_points[dig_in_channel_nums[i]][j] + durations[1]*30].reshape((-1, 30)), axis = 1)
         
-        	# Put the LFP data for this taste in hdf5 file under /Parsed_LFP
-        	hf5.create_array('/Parsed_LFP', 'dig_in_%i_LFPs' % (dig_in_channel_nums[i]), this_taste_LFPs)
-        	hf5.flush()
+        print (float(i)/len(dig_in_channels)) #Shows progress	
+    
+        # Put the LFP data for this taste in hdf5 file under /Parsed_LFP
+        hf5.create_array('/Parsed_LFP', 'dig_in_%i_LFPs' % (dig_in_channel_nums[i]), this_taste_LFPs)
+        hf5.flush()
         
 if trial_check == "No":
     num_electrodes = len(lfp_nodes) 
@@ -178,17 +185,17 @@ if trial_check == "No":
     for electrode in range(num_electrodes):
         this_taste_LFPs[electrode, 0, :] = np.mean(lfp_nodes[electrode][change_points[dig_in_channel_nums[0]][0] - durations[0]*30:change_points[dig_in_channel_nums[0]][0] + durations[1]*30].reshape((-1, 30)), axis = 1)
     
-    	# Put the LFP data for this session in hdf5 file under /Parsed_LFP
+        # Put the LFP data for this session in hdf5 file under /Parsed_LFP
     hf5.create_array('/Parsed_LFP', 'dig_in_%i_LFPs' % (dig_in_channel_nums[0]), this_taste_LFPs)
     hf5.flush()
-			
+            
 # Ask people if they want to delete rawLFPs or not, that way we offer the option to run analyses in many different ways. (ie. First half V back half)
 msg   = "Do you want to delete the Raw LFP data?"
 rawLFPdelete = easygui.buttonbox(msg,choices = ["Yes","No"])
 
 if rawLFPdelete == "Yes":
-	#Delete data
-	hf5.remove_node('/raw_LFP', recursive = True)
+    #Delete data
+    hf5.remove_node('/raw_LFP', recursive = True)
 hf5.flush()
 
 print("If you want to compress the file to release disk space, run 'blech_hdf5_repack.py' upon completion.")
--- LFP_analysis/Laser_LFP_Parse_Final.py
+++ LFP_analysis/Laser_LFP_Parse_Final.py
@@ -8,21 +8,29 @@ Created on Thu Jul  6 10:06:32 2017
 import numpy as np
 import easygui
 import tables
-import os
+import os, sys
 
-#Get name of directory where the data files and hdf5 file sits, and change to that directory for processing
-dir_name = easygui.diropenbox()
-os.chdir(dir_name)
+# Get name of directory where the data files and hdf5 file sits, 
+# and change to that directory for processing
+try:
+    dir_name = sys.argv[1]
+    if dir_name == '-f':
+        dir_name = easygui.diropenbox('Select the dir path where data are saved')
+except:
+    dir_name = easygui.diropenbox('Select the dir path where data are saved')
+
 
 #Look for the hdf5 file in the directory
 file_list = os.listdir('./')
 hdf5_name = ''
 for files in file_list:
-	if files[-2:] == 'h5':
-		hdf5_name = files
+    if files[-2:] == 'h5':
+        hdf5_name = files
 
 #Open the hdf5 file
 hf5 = tables.open_file(hdf5_name, 'r+')
+trains_dig_in = hf5.list_nodes('/spike_trains')
+lfp_nodes = hf5.list_nodes('/Parsed_LFP')
 
 # Create a LFP_laser group in the hdf5 file (if one exists, remove and create new)
 try:
@@ -31,16 +39,59 @@ except:
     pass
 hf5.create_group('/', 'LFP_Lasers')
 
-#Get laser conditions from hdf5 file (after ancillary analysis.py)
-laser_conditions = hf5.root.ancillary_analysis.laser_combination_d_l[:]
-laser_trials = hf5.root.ancillary_analysis.trials[:]
+# Ask the user about the type of units they want to do the calculations on (single or all units)
+unit_type = easygui.multchoicebox(msg = 'Which type of units do you want to use?', choices = ('All units', 'Single units', 'Multi units', 'Custom choice'))
+all_units = np.arange(trains_dig_in[0].spike_array.shape[1])
+single_units = np.array([i for i in range(len(all_units)) if hf5.root.unit_descriptor[i]["single_unit"] == 1])
+multi_units = np.array([i for i in range(len(all_units)) if hf5.root.unit_descriptor[i]["single_unit"] == 0])
+chosen_units = []
+if unit_type[0] == 'All units':
+    chosen_units = all_units
+elif unit_type[0] == 'Single units':
+    chosen_units = single_units
+elif unit_type[0] == 'Multi units':
+    chosen_units = multi_units
+else:
+    chosen_units = easygui.multchoicebox(msg = 'Which units do you want to choose?', choices = ([i for i in all_units]))
+    for i in range(len(chosen_units)):
+        chosen_units[i] = int(chosen_units[i])
+    chosen_units = np.array(chosen_units)
 
 #Get taste and LFP information
 num_tastes = len(hf5.list_nodes('/spike_trains'))
-lfp_nodes = hf5.list_nodes('/Parsed_LFP')
+# Now make arrays to pull the data out
+num_trials, _, time = trains_dig_in[0].spike_array.shape
+num_units = len(chosen_units)
+params = [250, 25]
+laser = np.empty(shape = (int((time - params[0])/params[1]) + 1, num_units, num_tastes*num_trials, 2), dtype = float)
 
-#Get trial information based on tastants (assumes that each taste has equal number of trials)
-num_trials = hf5.root.spike_trains.dig_in_0.spike_array[:].shape[0]
+# Now fill in the responses and laser (duration,lag) tuples
+for i in range(0, time - params[0] + params[1], params[1]):
+    for j in range(num_units):
+        for k in range(num_tastes):
+            # If the lasers were used, get the appropriate durations and lags. Else assign zeros to both
+            try:
+                laser[int(i/params[1]), j, num_trials*k:num_trials*(k+1)] = np.vstack((trains_dig_in[k].laser_durations[:], trains_dig_in[k].laser_onset_lag[:])).T
+            except:
+                laser[int(i/params[1]), j, num_trials*k:num_trials*(k+1)] = np.zeros((num_trials, 2))
+
+
+# First pull out the unique laser(duration,lag) combinations - these are the same irrespective of the unit and time
+unique_lasers = np.vstack({tuple(row) for row in laser[0, 0, :, :]})
+unique_lasers = unique_lasers[unique_lasers[:, 0].argsort(), :]
+unique_lasers = unique_lasers[unique_lasers[:, 1].argsort(), :]
+# Now get the sets of trials with these unique duration and lag combinations
+trials = []
+for i in range(len(unique_lasers)):
+    this_trials = [j for j in range(laser.shape[2]) if np.array_equal(laser[0, 0, j, :], unique_lasers[i, :])]
+    trials.append(this_trials)
+    trials = np.array(trials)
+laser_trials = trials.copy()
+laser_conditions = unique_lasers.copy()
+
+# #Get laser conditions from hdf5 file (after ancillary analysis.py)
+# laser_conditions = hf5.root.ancillary_analysis.laser_combination_d_l[:]
+# laser_trials = hf5.root.ancillary_analysis.trials[:]
 
 # Run through the tastes and laser conditions, and build arrays with respective data
 for x in range(laser_conditions.shape[0]):
